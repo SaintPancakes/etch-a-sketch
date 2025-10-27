4b58be6e22472661d7d4b5156ad38e80c6594617
Tim Jackson <221983337+SaintPancakes@users.noreply.github.com>
2025-10-27 10:31:33 +0000

    added remove, alternate grid generation and box values within button function/event listener

--- script.js
+++ script.js
@@ -5,8 +5,41 @@ sixteenth.classList.add('box');
 const fragment = new DocumentFragment();
 
 
-for(let i=0; i<16; i++) {
-    fragment.appendChild(sixteenth.cloneNode(true));
+for (let i = 0; i < 16; i++) {
+    const boxClone = sixteenth.cloneNode(true);
+
+    boxClone.addEventListener('mouseenter', () => {
+        boxClone.style.backgroundColor = 'red';
+    });
+
+    fragment.appendChild(boxClone);
 }
 
 gridContainer.appendChild(fragment);
+
+const button = document.createElement('button');
+button.addEventListener('click', () => {
+    const answer = prompt ("Number of squares per side for the new grid?");
+    box.remove();
+
+    for (let i = 0; i < $answer; i++) {
+    const boxClone = sixteenth.cloneNode(true);
+
+    boxClone.addEventListener('mouseenter', () => {
+        boxClone.style.backgroundColor = 'red';
+    });
+
+    fragment.appendChild(boxClone);
+}
+
+
+    const boxes = document.getElementsByClassName('box');
+        box.style.width = $1000/(Math.sqrt(answer));
+        box.style.height = $1000/(Math.sqrt(answer));
+        box.style.border = "solid 1px #000000";
+
+})
+
+document.body.appendChild(button)
+
+
